f8d745a4d221b9b1c0a70642009eeb243af0b626
Sheikh Abdul Halim <trainee.sheikh.abdul.halim@sigmalabs.co.uk>
2026-03-19 23:42:08 +0000

    Add basic html elements.

---
+++ app/main_ui.py
@@ -0,0 +1,13 @@
+"""Text-based browser game - Main UI server."""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('main_UI.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
